1a763f884c239126022386c93a867b95afd2d5bb
mileshammond <mileshammond81@hotmail.com>
2024-11-01 12:48:44 +0000

    Update wordle.py

--- wordle.py
+++ wordle.py
@@ -43,12 +43,13 @@ class wordle_tools():
         #Lowercase letters - Exist in the word but in the wrong place
         #lowercase letters prefixed with a dot - Dont exist in the word
 
+                #Look for letters in the correct places
                 for a in range(len(wordle)):
                     if user_input[a]==wordle[a]:  
                         #Letter guessed correctly
                         wordle_matrix[attempt][a]=user_input[a].upper()+" "
 
-                # Look for letters in the wrong place but are still used in the word
+                #Look for letters in the wrong place but are still used in the word
                 for a in range(len(wordle)):
                     #Checks letters that haven't been guessed correctly
                     if wordle_matrix[attempt][a]=='- ':
@@ -58,7 +59,7 @@ class wordle_tools():
                             wordle_formatted=str(wordle_matrix[attempt]).lower()
                             if wordle_formatted.count(user_input[a]) < wordle.count(user_input[a].lower()):
                                 wordle_matrix[attempt][a]=user_input[a].lower()+" "
-                            #If all occurences have been found then mark as incorrect guessed letter
+                            #If all occurences have been found then mark as incorrectly guessed letter
                             else:
                                 wordle_matrix[attempt][a]="."+user_input[a].lower()+" "        
                         else:
